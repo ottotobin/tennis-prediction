897517e129c844f32bbdafd7cf0b118667674f55
Toby Otto <totto@oberlin.edu>
2024-12-10 15:45:38 -0500

    getting needed attributes

--- datacleaning.py
+++ datacleaning.py
@@ -1,7 +1,16 @@
 import os
 import pandas as pd
 
-def process(dataset):
+def clean(dataset):
+    cleaned = dataset.copy()
+
+    # get just attributes need for models
+    cleaned = cleaned[["Tourney_name", "Surface", "Tourney_level", "Score", "Best_of", 
+    "Winner_name", "W_ace", "W_df", "W_svpt", "W_1stIn", "W_1stWon", "W_2ndWon", "W_bpSaved", "W_bpFaced",
+    "Loser_name", "L_ace", "L_df", "L_svpt", "L_1stIn", "L_1stWon", "L_2ndWon", "L_bpSaved", "L_bpFaced"]]
+
+    
+
     
 
 
@@ -9,7 +18,7 @@ def process(dataset):
 def main():
     for fn in os.listdir("./match-data"):
         dataset = pd.read_csv(fn)
-        process(dataset)
+        clean()
 
 
 
