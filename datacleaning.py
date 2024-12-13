2083e98b53d1fc6c3c0b2384043795c7f2c60b4b
Toby Otto <totto@oberlin.edu>
2024-12-13 18:50:35 -0500

    having trouble randomly assigning players

--- datacleaning.py
+++ datacleaning.py
@@ -28,27 +28,33 @@ L_STATS = ["l_ace", "l_df", "l_1stIn", "l_1stWon", "l_2ndWon", "l_bpSaved"]
 
     # cleaned["l_bpSaved%"] = cleaned.apply(lambda x: x["l_bpSaved"] / x["l_bpFaced"], axis=1)
 
+# creates a percentage if there exists a total
+def create_percentage(count, total):
+    if total != 0:
+        return count / total
+    else:
+        return 1
 
+def create_feature(player1, winner, w_percent, l_percent):
+    if player1 == winner:
+        return w_percent - l_percent
+    else:
+        return l_percent - w_percent
+
+# labels the match based on who won
+def create_label(player1, player2, winner):
+    if player1 == winner:
+        return "player1"
+    else:
+        return "player2"
 
 def clean(dataset):
-    def create_percentage(count, total):
-        if total != 0:
-            return count / total
-        else:
-            return None
-
-    def choose_player(player1, player2, winner):
-        if player1 == winner:
-            return "player1"
-        else:
-            return "player2"
-
     cleaned = dataset.copy()
 
-    # get just attributes need for models
+    # get just attributes needed for models
     cleaned = cleaned[ATTRIBUTES]
 
-    # create percentages
+    # create percentages for each winner and loser
     for w_count, l_count in zip(W_STATS, L_STATS):
         if "bp" in w_count:
             w_total = "w_bpFaced"
@@ -59,20 +65,35 @@ def clean(dataset):
         cleaned[w_count + "%"] = cleaned.apply(lambda x: create_percentage(x[w_count], x[w_total]), axis = 1)
         cleaned[l_count + "%"] = cleaned.apply(lambda y: create_percentage(y[l_count], y[l_total]), axis=1)
 
+
     # randomly assign winner and loser as Player 1 and Player 2
-    rand = random.random()
-    if rand < 0.5:
-        cleaned["player1"] = cleaned["winner_name"]
-        cleaned["player2"] = cleaned["loser_name"]
-    else:
-        cleaned["player1"] = cleaned["loser_name"]
-        cleaned["player2"] = cleaned["winner_name"]
+    cleaned["player1"] = [None for _ in range(cleaned.shape[0])]
+    cleaned["player2"] = [None for _ in range(cleaned.shape[0])]
+    for index, row in cleaned.iterrows():
+        rand = random.random()
+        print(rand)
+        if rand < 0.5:
+            print("player1 is winner")
+            row["player1"] = row["winner_name"]
+            row["player2"] = row["loser_name"]
+        else:
+            print("player2 is winner")
+            row["player2"] = row["winner_name"]
+            row["player1"] = row["loser_name"]
+
+    print(cleaned.head())
+    
+    # create features in the form: feature = stat_1 - stat_2
+    for w_count, l_count in zip(W_STATS, L_STATS):
+        w_percent = w_count + "%"
+        l_percent = l_count + "%"
+        feature_name = w_count[w_count.find("_"):] + "%"
+        cleaned[feature_name] = cleaned.apply(lambda x: create_feature(x["player1"], x["winner_name"], x[w_percent], x[l_percent]), axis=1)
 
     # create labels
-    cleaned["label"] = cleaned.apply(lambda x: choose_player(x["player1"], x["player2"], x["winner_name"]), axis=1)
+    cleaned["label"] = cleaned.apply(lambda x: create_label(x["player1"], x["player2"], x["winner_name"]), axis=1)
 
     # will pass each instance as a 3-tuple in the form: (player1, player2, instance)
-
     print(cleaned.head())
     return cleaned
 
@@ -80,6 +101,8 @@ def clean(dataset):
 #   encode surface
 #   encode best_of
 #   encode tourney_level
+
+
 
 
 def main():
