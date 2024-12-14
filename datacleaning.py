48d1ca55903f7d379617581b9672da195251e1c7
Toby Otto <totto@oberlin.edu>
2024-12-13 21:17:58 -0500

    completed datacleaning

--- datacleaning.py
+++ datacleaning.py
@@ -2,31 +2,12 @@ import os
 import pandas as pd
 import random
 
-ATTRIBUTES = ["tourney_name", "surface", "tourney_level", "score", "best_of", 
+ATTRIBUTES = ["surface", "tourney_level", "best_of", 
     "winner_name", "w_ace", "w_df", "w_svpt", "w_1stIn", "w_1stWon", "w_2ndWon", "w_bpSaved", "w_bpFaced",
     "loser_name", "l_ace", "l_df", "l_svpt", "l_1stIn", "l_1stWon", "l_2ndWon", "l_bpSaved", "l_bpFaced"]
 
 W_STATS = ["w_ace", "w_df", "w_1stIn", "w_1stWon", "w_2ndWon", "w_bpSaved"]
 L_STATS = ["l_ace", "l_df", "l_1stIn", "l_1stWon", "l_2ndWon", "l_bpSaved"]
-
-
-
-    # cleaned["w_ace%"] = cleaned.apply(lambda x: x["w_ace"] / x["w_svpt"], axis=1)
-    # cleaned["w_df%"] = cleaned.apply(lambda x: x["w_df"] / x["w_svpt"], axis=1)
-    # cleaned["w_1stIn%"] = cleaned.apply(lambda x: x["w_1stIn"] / x["w_svpt"], axis=1)
-    # cleaned["w_1stWon%"] = cleaned.apply(lambda x: x["w_1stWon"] / x["w_svpt"], axis=1)
-    # cleaned["w_2ndWon%"] = cleaned.apply(lambda x: x["w_2ndWon"] / x["w_svpt"], axis=1)
-
-    # cleaned["w_bpSaved%"] = cleaned.apply(lambda x: x["w_bpSaved"] / x["w_bpFaced"], axis=1)
-
-    # # create percentages for loser
-    # cleaned["l_ace%"] = cleaned.apply(lambda x: x["l_ace"] / x["l_svpt"], axis=1)
-    # cleaned["l_df%"] = cleaned.apply(lambda x: x["l_df"] / x["l_svpt"], axis=1)
-    # cleaned["l_1stIn%"] = cleaned.apply(lambda x: x["l_1stIn"] / x["l_svpt"], axis=1)
-    # cleaned["l_1stWon%"] = cleaned.apply(lambda x: x["l_1stWon"] / x["l_svpt"], axis=1)
-    # cleaned["l_2ndWon%"] = cleaned.apply(lambda x: x["l_2ndWon"] / x["l_svpt"], axis=1)
-
-    # cleaned["l_bpSaved%"] = cleaned.apply(lambda x: x["l_bpSaved"] / x["l_bpFaced"], axis=1)
 
 # creates a percentage if there exists a total
 def create_percentage(count, total):
@@ -34,6 +15,13 @@ def create_percentage(count, total):
         return count / total
     else:
         return 1
+
+def assign_players(winner, loser):
+    rand = random.random()
+    if rand < 0.5:
+        return winner
+    else:
+        return loser
 
 def create_feature(player1, winner, w_percent, l_percent):
     if player1 == winner:
@@ -48,11 +36,25 @@ def create_label(player1, player2, winner):
     else:
         return "player2"
 
+# hot-encodes categorical variables
+def encode(dataset):
+    dataset["best_of"] = dataset["best_of"].astype(object, copy=True)
+    categoricals = [column for column in dataset.select_dtypes(include=["object"]).columns if column not in ["winner_name", "loser_name", "player1", "player2", "label"]]
+    for column in dataset.columns:
+        if column in categoricals:    
+            onehots = pd.get_dummies(dataset[column], column, drop_first=False, dtype=int)
+            dataset = pd.concat([dataset.drop(column, axis=1), onehots], axis=1)
+
+    return dataset
+
 def clean(dataset):
     cleaned = dataset.copy()
 
     # get just attributes needed for models
     cleaned = cleaned[ATTRIBUTES]
+
+    # get just ATP matches at Grand Slam(G), Masters 1000(M), and Tour-level(A)
+    cleaned = cleaned[cleaned["tourney_level"].isin(["G", "M", "A"])]
 
     # create percentages for each winner and loser
     for w_count, l_count in zip(W_STATS, L_STATS):
@@ -67,52 +69,38 @@ def clean(dataset):
 
 
     # randomly assign winner and loser as Player 1 and Player 2
-    cleaned["player1"] = [None for _ in range(cleaned.shape[0])]
-    cleaned["player2"] = [None for _ in range(cleaned.shape[0])]
-    for index, row in cleaned.iterrows():
-        rand = random.random()
-        print(rand)
-        if rand < 0.5:
-            print("player1 is winner")
-            row["player1"] = row["winner_name"]
-            row["player2"] = row["loser_name"]
-        else:
-            print("player2 is winner")
-            row["player2"] = row["winner_name"]
-            row["player1"] = row["loser_name"]
+    cleaned["player1"] = cleaned.apply(lambda x: assign_players(x["winner_name"], x["loser_name"]), axis=1)
+    cleaned["player2"] = cleaned.apply(lambda x: x["loser_name"] if x["player1"] == x["winner_name"] else x["winner_name"], axis=1)
 
-    print(cleaned.head())
-    
     # create features in the form: feature = stat_1 - stat_2
     for w_count, l_count in zip(W_STATS, L_STATS):
         w_percent = w_count + "%"
         l_percent = l_count + "%"
-        feature_name = w_count[w_count.find("_"):] + "%"
+        feature_name = w_count[w_count.find("_") + 1:] + "%"
         cleaned[feature_name] = cleaned.apply(lambda x: create_feature(x["player1"], x["winner_name"], x[w_percent], x[l_percent]), axis=1)
 
     # create labels
     cleaned["label"] = cleaned.apply(lambda x: create_label(x["player1"], x["player2"], x["winner_name"]), axis=1)
 
+    # removed winner and loser count stats
+    removables = W_STATS + L_STATS + [stat + "%" for stat in W_STATS + L_STATS] + ["w_svpt", "w_bpFaced", "l_svpt", "l_bpFaced", "winner_name", "loser_name"]
+    
+    cleaned = cleaned.drop(removables, axis=1)
+
     # will pass each instance as a 3-tuple in the form: (player1, player2, instance)
-    print(cleaned.head())
     return cleaned
-
-# process
-#   encode surface
-#   encode best_of
-#   encode tourney_level
-
-
 
 
 def main():
-    # for fn in os.listdir("./raw-matchdata"):
-    #     dataset = pd.read_csv(fn)
-    #     clean(dataset)
+    print(os.listdir("./raw-matchdata"))
+    for rawfile in os.listdir("./raw-matchdata"):
+        matchfile = "./raw-matchdata/" + rawfile
+        dataset = pd.read_csv(matchfile)
 
-    dataset = pd.read_csv("./raw-matchdata/atp_matches_2013.csv")
-    # print(dataset.iloc[0])
-    cleaned = clean(dataset)
+        cleaned = clean(dataset)
+        encoded = encode(cleaned)
+        processedfile = "./processed-matchdata/" + rawfile.strip("_")
+        encoded.to_csv(processedfile)
 
 
 if __name__ == "__main__":
